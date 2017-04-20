a69f61d89cf270cc28488750bb6563e10af171bf
wei <weizongwei@weideMacBook-Air.local>
2017-04-20 11:59:06 +0800

    修改logger.c

--- GuetzliIOS/GuetzliIOS/libguetzli/logger.c
+++ GuetzliIOS/GuetzliIOS/libguetzli/logger.c
@@ -3,25 +3,26 @@
 
 
 #define LOG_TAG "JNILOG" // 这个是自定义的LOG的标识
-
+#import <Foundation/Foundation.h>
 namespace{
 
 void printD(const char *str)
 {
-    
-    //LOGD("%s", str);
+    NSLog("🌑 %s:%s",LOG_TAG, str);
 }
 
 
 void printI(const char *str)
 {
     
+    NSLog("🔵 %s:%s",LOG_TAG, str);
+    
 }
 
 
 void printE(const char *str)
 {
-    
+    NSLog("⭕ %s:%s",LOG_TAG, str);
 }
 
 }
